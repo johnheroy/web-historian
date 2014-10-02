70a9c2504db3edc682fef57130c46108d0b1a82c
johnheroy <johnheroy@gmail.com>
2014-10-01 19:01:14 -0700

    completed html fetcher

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,24 @@
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
+var archiveHelpers = require('../helpers/archive-helpers');
+var httpRequest = require('http-request');
+var fs = require('fs');
+
+archiveHelpers.readListOfUrls(function(urlList){
+  urlList.forEach(function(url){
+    if (url){
+      httpRequest.get('http://' + url, function(err, res){
+        if (err) throw err;
+        if (res.code === 200){
+          console.log(archiveHelpers.paths['archivedSites'] + '/' + url);
+          fs.writeFile(archiveHelpers.paths['archivedSites'] + '/' + url, res.buffer.toString(), function(err){
+            if (err) throw err;
+            console.log('saved html for', url);
+          });
+        }
+      });
+    }
+  });
+  // for (var i = 0; i < urlList.length; i++){
+  // }
+});
